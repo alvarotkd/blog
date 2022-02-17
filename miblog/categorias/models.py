c1b7b2a0eaff9172428966af135a5a9a81866b1f
alvaro <alvaro_tkd5@hotmail.com>
2022-02-17 12:55:06 -0300

    modelo de categorias

--- miblog/categorias/models.py
+++ miblog/categorias/models.py
@@ -1,3 +1,15 @@
+from statistics import mode
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class categorias (models.Model):
+    nombre = models.CharField(max_length= 100, default='Value')
+    detalles= models.CharField(max_length=1000, default='vaule')
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now_add=True)
+
+    class meta:
+        db_table= 'categorias'
+
+
+
